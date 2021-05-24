32c6c4d89722a482d4468552985495436415a6d6
EC2 Default User <ec2-user@ip-172-31-24-0.ec2.internal>
2021-05-24 11:32:07 +0000

    Avatar ready

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -64,6 +64,6 @@ class UsersController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def user_params
-      params.require(:user).permit(:firstname, :lastname, :email)
+      params.require(:user).permit(:firstname, :lastname, :email, :avatar)
     end
 end
